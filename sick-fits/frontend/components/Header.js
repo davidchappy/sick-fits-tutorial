57aa04bea20e97385ad02852e6b093b022808204
David Chapman <davidchappy@gmail.com>
2019-05-28 12:31:50 -0500

    Finish videos 7-9

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,6 +1,18 @@
 import styled from "styled-components"
 import Link from "next/link"
+import Router from "next/router"
+import NProgress from "nprogress"
 import Nav from "./Nav"
+
+Router.onRouteChangeStart = () => {
+  NProgress.start()
+}
+Router.onRouteChangeComplete = () => {
+  NProgress.done()
+}
+Router.onRouteChangeError = () => {
+  NProgress.done()
+}
 
 const Logo = styled.h1`
   font-size: 4rem;
@@ -38,7 +50,7 @@ const StyledHeader = styled.header`
   .sub-bar {
     display: grid;
     grid-template-columns: 1fr auto;
-    border-bottom: 1px solid ${props => props.theme.lightgray};
+    border-bottom: 1px solid ${props => props.theme.lightgrey};
   }
 `
 
