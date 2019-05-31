38a3a139d01ab82de3b81ea4001287d45f9a4243
David Chapman <davidchappy@gmail.com>
2019-05-31 14:36:56 -0500

    Complete 24

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const mutations = {
   // checked if logged in
   async createItem(parent, args, ctx, info) {
@@ -36,6 +39,31 @@ const mutations = {
 
     // delete it
     return ctx.db.mutation.deleteItem({ where }, info)
+  },
+
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase()
+
+    // hash their password
+    const password = await bcrypt.hash(args.password, 10)
+    // create user in db
+    const user = await ctx.db.mutation.createUser({
+      data: {
+        ...args,
+        password,
+        permissions: { set: ['USER'] }
+      },
+      info
+    })
+    // Create JWT token for them
+    const token = jwt.sign({ userID: user.id }, process.env.APP_SECRET)
+    // Set the JWT as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    })
+    // Return the user to the browser
+    return user
   }
 };
 
